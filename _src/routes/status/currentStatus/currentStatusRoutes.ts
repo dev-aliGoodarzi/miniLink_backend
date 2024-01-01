15bbcf80c804a7236e60cba8861fbec53bcb4a0b
Ali Goodarzi <me74242@gmail.com>
2024-01-02 01:51:22 +0330

    #

--- _src/routes/status/currentStatus/currentStatusRoutes.ts
+++ _src/routes/status/currentStatus/currentStatusRoutes.ts
@@ -2,8 +2,17 @@
 import express from "express";
 // Express
 
-import { Systeminformation } from "systeminformation";
+const si = require("systeminformation");
 
 export const currentStatusRoutes = express.Router();
 
-currentStatusRoutes.get("/current-status", (req, res) => {});
+currentStatusRoutes.get("/current-status", async (req, res) => {
+  si.cpu()
+    .then((data: any) => {
+      res.status(200).json(data);
+    })
+
+    .catch((err: any) => {
+      res.status(413).json(err);
+    });
+});
